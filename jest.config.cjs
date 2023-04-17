39480ac09ec3d02843ca52cf46733bf5b9dc6a93
Fernando Nicolás <fersanti2896@gmail.com>
2023-04-17 15:19:14 -0600

    Instalaciones de test

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,8 @@
 module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js'],
-    transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"]
+    transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
+    // moduleNameMapper: {
+    //     '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+    // }
 }
