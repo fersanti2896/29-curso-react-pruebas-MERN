14eafe99f090fb655de3a69d6446debb77c138b1
Fernando Nicolás <fersanti2896@gmail.com>
2023-04-30 23:30:58 -0600

    Pruebas en useUIStore.js

--- tests/hooks/useUIStore.test.js
+++ tests/hooks/useUIStore.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useUIStore } from '../../src/hooks/useUIStore';
 import { Provider } from 'react-redux';
 import { store } from '../../src/store';
@@ -29,6 +29,40 @@ describe('Pruebas en el hook useUIStore', () => {
             isDateModalOpen: false,
             openDateModal: expect.any( Function ),
             closeDateModal: expect.any( Function )
-        })
+        });
+    });
+
+    test('openDateModal debe de colocar true en el isDateModalOpen.', () => { 
+        /* Estado inicial */
+        const mockStore = getMockStore({ isDateModalOpen: false });
+        /* Monstamos el hook */
+        const { result } = renderHook( () => useUIStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        } );
+
+        const { isDateModalOpen, openDateModal } = result.current;
+        
+        act( () => {
+            openDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeTruthy();
+    });
+
+    test('closeDateModal debe de colocar un false en el isDateModalOpen.', () => { 
+        /* Estado inicial */
+        const mockStore = getMockStore({ isDateModalOpen: true });
+        /* Monstamos el hook */
+        const { result } = renderHook( () => useUIStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        } );
+
+        const { isDateModalOpen, closeDateModal } = result.current;
+
+        act( () => {
+            closeDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeFalsy();
     });
 });
