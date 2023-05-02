a3062f03da10d858398eb5e8be4a4db5ce819fc1
Fernando Nicolás <fersanti2896@gmail.com>
2023-05-02 01:38:54 -0600

    Pruebas en AppRouter.jsx

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
     testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js'],
     transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
-    // moduleNameMapper: {
-    //     '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
-    // }
+    moduleNameMapper: {
+        '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
+    },
     setupFilesAfterEnv: [
         '<rootDir>/tests/mocks/envValues.js'
       ]
